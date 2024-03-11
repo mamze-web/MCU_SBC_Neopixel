f7293baea737162daa6d1c597e83935430849202
mamze-web <mamze0109@naver.com>
2024-03-11 16:36:35 +0900

    firstFile

---
+++ README.md
@@ -0,0 +1,55 @@
+![img](https://cdn-shop.adafruit.com/970x728/1138-00.jpg)
+
+[![YouTube Channel Views](https://img.shields.io/youtube/channel/views/UCz5BOU9J9pB_O0B8-rDjCWQ?label=YouTube&style=social)](https://www.youtube.com/watch?v=tQJWNoRK7sc)
+
+# 막대 조명 만들기
+
+[조명](https://ko.wikipedia.org/wiki/조명)의 개념을 활용해서 아두이노로 막대조명을 만들어볼 것이다.
+
+# 하드웨어 
+
+- 아두이노 우노  
+- ada-1461
+- M-M 점퍼선 8개 
+- USB 2.0 케이블 Type A/B
+
+
+**주의: 점퍼선 연결시 케이블을 제거하고 진행합니다.**
+
+## 선 작업 
+
+| [Arduino UNO GPIO](https://docs.arduino.cc/resources/pinouts/A000066-full-pinout.pdf) | ada-1461 |
+|-----------|------|
+|   D8     | DI  |
+|   5V      | 5V  |
+|   GND     | GND  |
+
+## 구입처 
+
+You can purchase ada-1461 through this link [here](https://www.devicemart.co.kr/goods/view?no=1274050)
+
+# 코드 다운로드 
+
+    git clone https://
+
+## 라이브러리  
+1. Library 
+2. Search
+3. Install
+
+## 컴파일   
+
+1. open
+2. compile
+
+## 업로드  
+1. upload
+
+# 동작영상 
+
+Here's a video outlining how it works and a timelapse of it looking after a plant for a week:
+
+[![Mod demo](https://img.youtube.com/vi/E6wkvTG2Ofs/0.jpg)](https://www.youtube.com/watch?v=E6wkvTG2Ofs "Video Title")
+
+# 라이센스 
+[GPL 3.0](https://olis.or.kr/license/Detailselect.do?lId=1072&mapCode=010072)
---
+++ neopixel.ino
@@ -0,0 +1,54 @@
+#include <Adafruit_NeoPixel.h>
+
+// 네오픽셀을 연결한 핀과 네오픽셀 개수 정의
+#define LED_PIN 8
+#define LED_COUNT 60
+
+// Adafruit_NeoPixel 라이브러리를 사용하여 네오픽셀 객체 생성
+Adafruit_NeoPixel strip(LED_COUNT , LED_PIN, NEO_GRB + NEO_KHZ800);
+
+// 초기 설정
+void setup(){
+    // 네오픽셀 초기화
+    strip.begin();
+
+    // 네오픽셀 초기화 후 모든 네오픽셀을 끔
+    strip.show();
+
+    // 네오픽셀 밝기 설정 (0 ~ 255 범위)
+    strip.setBrightness(50); // 50% 밝기
+}
+
+// 메인 루프
+void loop(){
+    // colorWipe 함수 호출하여 빨간색으로 네오픽셀을 채우는 효과 생성
+    colorWipe(strip.Color(255, 0 , 0), 50); // 빨간색, 50ms 딜레이
+
+    // rainbow 함수 호출하여 네오픽셀에 무지개 효과 생성
+    rainbow(10); // 10ms 딜레이
+}
+
+// 지정된 색으로 네오픽셀을 채우는 함수
+void colorWipe(uint32_t color, int wait){
+    for (int i = 0; i < strip.numPixels(); i++){ㅁㄴ
+        strip.setPixelColor(i, color); // i번째 네오픽셀의 색을 설정
+
+        strip.show(); // 변경된 색상을 네오픽셀에 표시
+
+        delay(wait); // 설정한 시간만큼 대기
+    }
+}
+
+// 무지개 효과를 생성하는 함수
+void rainbow(int wait) {  
+    // 모든 네오픽셀에 대해 무지개 색상을 계산하여 설정
+    for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256) {
+        for (int i = 0; i < strip.numPixels(); i++) { 
+            // 네오픽셀 위치에 따라 무지개 색상 결정
+            int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());      
+            strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+        }
+        strip.show(); // 변경된 색상을 네오픽셀에 표시
+        delay(wait); // 설정한 시간만큼 대기
+    }
+}
